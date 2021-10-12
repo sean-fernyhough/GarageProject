055eec436d634a88716905f2470d56f3c954e563
sean-fernyhough <sean.fernyhough97@gmail.com>
2021-10-12 12:38:59 +0100

    completed garage class methods

--- src/com/qa/model/Garage.java
+++ src/com/qa/model/Garage.java
@@ -14,10 +14,19 @@ public class Garage {
 		garage.add(vehicle);
 	}
 	
-	public float fix(String id) {
+	public float fixDiff(String id) {
 		float value = 0;
 		for(int i = 0; i < garage.size(); i++ ) {
 			if (garage.get(i).getId() == id) {
+				Vehicle vehicle = garage.get(i);
+				String type = vehicle.getType();
+					if(type == "car") {
+						float subValue = vehicle.calcValue() * 3 ;
+					}else if (type == "motorbike") {
+						float subValue = vehicle.calcValue() * 0.8f;
+					}else if (type == "speed boat") {
+						float subValue = vehicle.calcValue() * 10;
+					}
 				float subValue = garage.get(i).calcValue();
 				return subValue;
 			}
@@ -25,9 +34,66 @@ public class Garage {
 		return value;
 		}
 	
+	public float fix(String id) {
+		float value = 0;
+		for(int i = 0; i < garage.size(); i++ ) {
+			if (garage.get(i).getId() == id) {
+				Vehicle vehicle = garage.get(i);
+				float subValue = garage.get(i).calcValue();
+				return subValue;
+			}
+			}
+		return value;
+		}
+	
+	public float fixAll(String id) {
+		float value = 0;
+		for(int i = 0; i < garage.size(); i++ ) {
+				value += garage.get(i).calcValue();
+				return value;
+			}
+		return value;
+			}
+
+
+	
+	
+
+	public void removeId(String id) {
+		for(int i = 0; i < garage.size(); i++ ) {
+			if (garage.get(i).getId() == id) {
+				garage.remove(i);
+			}
+			}
+	}
+	
+	public void removeTypeAll(String type) {
+		for(int i = 0; i < garage.size(); i++ ) {
+			if (garage.get(i).getType() == type) {
+				garage.remove(i);
+			}
+			}
+	}
+	
+	public void removeTypeSingle(String type) {
+		for(int i = 0; i < garage.size(); i++ ) {
+			if (garage.get(i).getType() == type) {
+				garage.remove(i);
+				break;
+			}
+			}
+	}
+	
+	
+	public void removeIndex(int index) {
+				garage.remove(index);
+			}
+	
+	
 	public void empty() {
 		garage.clear();
 	}
+	
 		
 	}
 	
