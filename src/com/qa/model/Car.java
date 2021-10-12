acd7e6656950ee3afb161bd99b09317ae7c4c359
sean-fernyhough <sean.fernyhough97@gmail.com>
2021-10-12 13:11:15 +0100

    created subclasses

---
+++ src/com/qa/model/Car.java
@@ -0,0 +1,33 @@
+package com.qa.model;
+
+public class Car extends Vehicle {
+
+	private int numDoors;
+	private int bootSpace;
+	private boolean hasSeatWarmers;
+	
+	
+	public Car(String id, String type, String colour, float engineSize, boolean isPreowned, int age, int topSpeed,
+			float acceleration, int baseValue, int numDoors, int bootSpace, boolean hasSeatWarmers) {
+		super(id, type, colour, engineSize, isPreowned, age, topSpeed, acceleration, baseValue);
+		this.numDoors = numDoors;
+		this.bootSpace = bootSpace;
+		this.hasSeatWarmers = hasSeatWarmers;
+	}
+	
+	@Override
+	public float calcValue() {
+		float value = getBaseValue() + (getEngineSize() * 2000) + (getTopSpeed() * 10) + (getAcceleration() * 30) + (numDoors * 500) + (bootSpace * 100);
+		value -= (getAge() * 80);
+		if(hasSeatWarmers) {
+			value += 2000;
+		}
+		if(isPreowned()) {
+			value -= value * 0.6;
+			return value;
+		}else {
+		return value;
+		}
+	}
+
+}
---
+++ src/com/qa/model/Motorbike.java
@@ -0,0 +1,36 @@
+package com.qa.model;
+
+public class Motorbike extends Vehicle {
+
+	private int wheels;
+	private boolean hasPassengerSeats;
+	private boolean hasMudguards;
+	
+	public Motorbike(String id, String type, String colour, float engineSize, boolean isPreowned, int age, int topSpeed,
+			float acceleration, int baseValue, int wheels, boolean hasPassengerSeats, boolean hasMudguards) {
+		super(id, type, colour, engineSize, isPreowned, age, topSpeed, acceleration, baseValue);
+		this.wheels = wheels;
+		this.hasPassengerSeats = hasPassengerSeats;
+		this.hasMudguards = hasMudguards;
+	}
+	
+	
+	@Override
+	public float calcValue() {
+		float value = getBaseValue() + (getEngineSize() * 2000) + (getTopSpeed() * 10) + (getAcceleration() * 30) + (wheels * 500);
+		value -= (getAge() * 80);
+		if(hasMudguards) {
+			value += 500;
+		}
+		if(hasPassengerSeats) {
+			value += 1000;
+		}
+		if(isPreowned()) {
+			value -= value * 0.6;
+			return value;
+		}else {
+		return value;
+		}
+	}
+
+}
