47949dd3efb44deb57eda7bbff6fe79370e21af7
sean-fernyhough <sean.fernyhough97@gmail.com>
2021-10-12 11:44:52 +0100

    created vehicle class constructor and value calculator method

--- src/com/qa/model/Vehicle.java
+++ src/com/qa/model/Vehicle.java
@@ -2,10 +2,28 @@ package com.qa.model;
 
 public class Vehicle {
 
-	private
+	private String id;
+	private String type;
+	private String colour;
+	private float engineSize;
+	private boolean isPreowned;
+	private int age;
+	private int topSpeed;
+	private float acceleration;
+	private int baseValue;
 	
-	public Vehicle() {
-		
+	
+	public Vehicle(String id, String type, String colour, float engineSize, boolean isPreowned, int age, int topSpeed, float acceleration, int baseValue) {
+		this.id = id;
+		this.type = type;
+		this.colour = colour;
+		this.engineSize = engineSize;
+		this.isPreowned = isPreowned;
+		this.age = age;
+		this.topSpeed = topSpeed;
+		this.acceleration = acceleration;
+		this.baseValue = baseValue;
 	}
+	
 	
 }
